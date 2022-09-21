00f110c1ca664f9508e785c466166b78bf2e8ae3
ammylovetodie <er.amritpal88@gmail.com>
2022-09-21 21:40:30 +0100

    Update user_list.py

--- user_list.py
+++ user_list.py
@@ -21,36 +21,62 @@ combine_list = combine_random_list_fun("group2","group3")
 
 for user in random_list_fun("group1"):
   print(user,combine_list[loop])
+  if user in user_list["group2"]:
+    user_list["group1"].remove(user)
+  if combine_list[loop] in user_list["group3"]:
+    user_list["group3"].remove(combine_list[loop])
+  if combine_list[loop] in user_list["group2"]:
+    user_list["group2"].remove(combine_list[loop])
   loop = loop + 1
 
-group1_element=len(user_list['group1'])
-
-###remove used element
-#def remove_element(first_group,second_group):
-counter = 0
-while counter != group1_element:
-  if combine_list[counter] in user_list["group2"]:
-    user_list["group2"].remove(combine_list[counter])
-  elif combine_list[counter] in user_list["group3"]:
-    user_list["group3"].remove(combine_list[counter])
-  counter = counter + 1
-
-#remove_element("group2","group3")
 #####
 def rest_element_compare(first_list,second_list):
   loop=0
   combine_list = random_list_fun(second_list)
   for user in random_list_fun(first_list):
-    print(user,combine_list[loop])
+    print("data",user,combine_list[loop])
+    if user in user_list["group2"]:
+      user_list["group2"].remove(user)
+    if combine_list[loop] in user_list["group3"]:
+      user_list["group3"].remove(combine_list[loop])
     loop = loop + 1
+
+def randon_pair_with_rest_element(list_name):
+  # print(user_list[list_name])
+  loop=0
+  while len(user_list[list_name]) > 2:
+  #for user in user_list[list_name]:
+    print(user_list[list_name][0],user_list[list_name][1])
+    user_list[list_name].remove(user_list[list_name][0])
+    user_list[list_name].remove(user_list[list_name][0])
+    #loop = loop + 1
+  print(user_list[list_name])
+  if len(user_list[list_name]) == 2:
+    print(user_list[list_name][0],user_list[list_name][1])
+    user_list[list_name].remove(user_list[list_name][0])
+    user_list[list_name].remove(user_list[list_name][0])
+  elif len(user_list[list_name]) == 1:
+    print(user_list[list_name][0],user_list[list_name][1])
+    user_list[list_name].remove(user_list[list_name][0])
+  elif len(user_list[list_name]) == 3:
+    print(user_list[list_name][0],user_list[list_name][1],user_list[list_name][2])
+    user_list[list_name].remove(user_list[list_name][0])
+    user_list[list_name].remove(user_list[list_name][0])
+    user_list[list_name].remove(user_list[list_name][0])
 
 ##checking the length of the list
 if len(user_list['group2']) < len(user_list['group3']):
   print("group2 having less users",user_list['group2'])
+  print("group3 having less users",user_list['group3'])
   print(rest_element_compare("group2","group3"))
+  #print(user_list["group3"])
+  randon_pair_with_rest_element("group3")
 elif len(user_list['group2']) > len(user_list['group3']):
   print("group3 having less users",user_list['group3'])
+  print("group2 having less users",user_list['group2'])
   print(rest_element_compare("group3","group2"))
+  #print(user_list["group2"])
+  randon_pair_with_rest_element("group2")
 elif len(user_list['group2']) == len(user_list['group3']):
   print("both having same users")
   print(rest_element_compare("group2","group3"))
